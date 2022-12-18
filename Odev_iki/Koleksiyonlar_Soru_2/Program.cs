2747430863ae4612fc299710accc9a88cf6877b0
cemkndz <120122784+ckkurs@users.noreply.github.com>
2022-12-18 19:39:38 +0300

    Odev_İki_Patkia

---
+++ Odev_iki/Koleksiyonlar_Soru_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Koleksiyonlar_Soru_2
+{
+    class Program
+    {
+         //Klavyeden girilen 20 adet sayının en büyük 3 tanesi ve en küçük 3 tanesi bulan, her iki grubun kendi içerisinde ortalamalarını alan ve bu ortalamaları ve ortalama toplamlarını console'a yazdıran programı yazınız. (Array sınıfını kullanarak yazınız.)
+        static  void Main(string[] args)
+        {
+            int[] sayilar= new int[20];
+            int[] enkucuksayi=new int[3];
+            int[] enbuyuksayi=new int[3];
+
+            int enkucuklerortalama,enkucuklertoplam=0;
+            int enbuyuklerortalama,enbuyuklertoplam=0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                 Console.WriteLine("{0}. Pozitif Sayı Giriniz: ",i+1);
+                 int n=int.Parse(Console.ReadLine());
+                 sayilar[i]=n;
+            }
+            
+            Array.Sort(sayilar);
+
+            for (int j = 0; j < 3; j++)
+            {
+                enkucuksayi[j]=sayilar[j];
+                enkucuklertoplam=enkucuklertoplam+enkucuksayi[j];
+            }
+
+            Console.WriteLine("***En kucuk Sayılar***");
+            foreach (var item in enkucuksayi)
+            {
+                Console.WriteLine(item);
+            }
+
+            Array.Reverse(sayilar);
+
+            for (int k = 0; k < 3; k++)
+            {
+                enbuyuksayi[k]=sayilar[k];
+                enbuyuklertoplam=enbuyuklertoplam+enbuyuksayi[k];
+            }
+
+            Console.WriteLine("***En ;Büyük Sayılar");
+
+            foreach (var item in enbuyuksayi)
+            {
+                Console.WriteLine(item);
+            }
+            
+            enkucuklerortalama=enkucuklertoplam/3;
+            Console.WriteLine("En küçük üç sayının Ortalaması: {0}",enkucuklertoplam/3);
+
+            enbuyuklerortalama=enbuyuklertoplam/3;
+            Console.WriteLine("En büyük üç sayının Ortalaması: {0}",enbuyuklertoplam/3);
+
+            
+
+            Console.WriteLine("Ortalama toplamları: {0}",enbuyuklerortalama+enkucuklerortalama);
+            
+        
+        }
+    }
+}
---
+++ Odev_iki/Koleksiyonlar_Soru_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+
+        //Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir Cümle Giriniz: ");
+            string cumle=Console.ReadLine();
+
+            ArrayList sesli=new ArrayList();
+
+            string sesliHarfler = "aeıioöuü";
+
+          if(!string.IsNullOrEmpty(cumle))
+          {
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                if(sesliHarfler.Contains(cumle[i]))
+                {
+                    sesli.Add(cumle[i]);
+                }
+
+            }
+          }
+          
+            sesli.Sort();
+            
+            foreach (var item in sesli)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
